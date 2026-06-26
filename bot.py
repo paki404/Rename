7d99fb9ee4af6df8cf65753aa4296ae681848778
paki404 <pakistanikira4@gmail.com>
2026-06-26 14:27:33 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,11 @@
 import os
-import asyncio
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Application, CommandHandler, MessageHandler, CallbackQueryHandler, ContextTypes, filters
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🔥 Bot On hai\nMujhe MEGA folder ka link bhejo.")
+    await update.message.reply_text("🔥 Bot On hai")
 
 app = Application.builder().token(BOT_TOKEN).build()
 app.add_handler(CommandHandler("start", start))
